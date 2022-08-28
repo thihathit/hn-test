136d72feee10bae52f838b309889bf7bcd602cb9
Thiha Thit <developer.thiha@gmail.com>
2022-08-28 20:23:13 +0700

    feat: add vote button

--- src/components/blocks/Comments.vue
+++ src/components/blocks/Comments.vue
@@ -1,16 +1,24 @@
 <script setup lang="ts">
+import { ref } from "vue"
 import type { TComments } from "$types/hn"
 
+const vote = ref(0)
 const props = defineProps<{ data: TComments }>()
 
 const commentTitle = `Comment${props.data.comments.length > 1 ? "s" : ""}`
 const hasComments = !!props.data.comments.length
+
+const onVote = () => {
+    vote.value++
+}
 </script>
 
 <template>
     <details open>
         <summary>
             <mark v-if="data.user">{{ data.user }}</mark> {{ data.time_ago }}
+            {{ " " }}
+            <button @click="onVote">Vote: {{ vote }}</button>
         </summary>
 
         <article v-html="data.content" />
